3323bb7669eb1882045bcb550733f77c32dead9b
Petter Sandholdt <petter.sandholdt@sectra.com>
2026-01-22 14:50:04 +0100

    Refactor Multical21 component configuration and update .gitignore for ESPHome

--- esphome/components/multical21/__init__.py
+++ esphome/components/multical21/__init__.py
@@ -21,14 +21,13 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(Multical21Component),
-            cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
             cv.Required(CONF_GDO0_PIN): pins.gpio_input_pin_schema,
             cv.Required(CONF_METER_ID): cv.string,
             cv.Required(CONF_KEY): cv.string,
         }
     )
     .extend(cv.polling_component_schema("1s"))
-    .extend(spi.spi_device_schema(cs_pin_required=False, default_mode="MODE0", default_data_rate=1e6))
+    .extend(spi.spi_device_schema(cs_pin_required=True))
 )
 
 
@@ -36,9 +35,6 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield spi.register_spi_device(var, config)
-
-    cs_pin = yield cg.gpio_pin_expression(config[CONF_CS_PIN])
-    cg.add(var.set_cs_pin(cs_pin))
 
     gdo0_pin = yield cg.gpio_pin_expression(config[CONF_GDO0_PIN])
     cg.add(var.set_gdo0_pin(gdo0_pin))
